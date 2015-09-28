40f9271ef3cedac04dfa950022df97bd46e680f9
Andras Buday <rise.hun@gmail.com>
2015-09-28 13:52:54 +0000

    Added FruitDashboard class

---
+++ FruitDashboard.php
@@ -0,0 +1,72 @@
+<?php
+
+class FruitDashboard {
+
+    /**
+     * The API key.
+     *
+     * @var string
+     */
+    protected $apiKey;
+
+    /**
+     * The API version.
+     *
+     * @var string
+     */
+    protected $apiVersion;
+
+    /**
+     * Constructor
+     *
+     * @param string $apiKey
+     * @param string $apiVersion
+     */
+    public function __construct($apiKey, $apiVersion) {
+        $this->apiKey = $apiKey;
+        $this->apiVersion = $apiVersion;
+    }
+
+    /**
+     * Creates the post url.
+     *
+     * @param int $widgetId
+     * @return string
+     */
+    private function buildPostUrl($widgetId) {
+        $url = 'https://fd-stg.tryfruit.com';
+        $url .= '/api/' . $this->apiVersion;
+        $url .= '/' . $this->apiKey;
+        $url .= '/' . $widgetId;
+        return $url;
+    }
+
+    /**
+     * post
+     * Sending the request to the API.
+     * --------------------------------------------------
+     * @param string $postData
+     * @return response
+     * --------------------------------------------------
+    */
+    public function post($postData) {
+        // Popping widget id from data.
+        if ( ! array_key_exists('widgetId', $postData)) {
+            $widgetId = 0;
+        } else {
+            $widgetId = $postData['widgetId'];
+            unset($postData['widgetId']);
+        }
+
+        // Setting up curl.
+        $ch = curl_init($this->buildPostUrl($widgetId));
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($postData));
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+
+        // Executing query.
+        return json_decode(curl_exec( $ch ), 1);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# fruitdashboard-api
+# Fruitdashboard API
+
+## Installation
+
+Copy the FruitDashboard class somewhere into your project.
+
+## Usage
+
+Here's a code snippet of how you can use the API.
+
+### Preparing the Data
+```
+$myData = array(
+    'widgetId'          => your-widget-id, // can be found on the widget
+    'date'              => 'yyyy-mm-dd',
+    'my_first_dataset'  => 1,
+    'my_second_dataset' => 2
+);
+```
+
+### Instantiating the class.
+```
+$fruitDashboard = new FruitDashboard('your-api-key', 'api-version');
+$response = $fruitDashboard->post($myData);
+```
+
+### Handling response.
+```
+    // Printing message from server.
+    if ($response['status'] === FALSE) {
+        // An error occurred.
+        echo 'Something went wrong. This is what we know: ';
+    }
+    echo $response['message'];
+```
+
+That would be all of it.
